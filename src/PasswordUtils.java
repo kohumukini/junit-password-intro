4102309f4bef43859e01db758360f81a9d87fb97
kohumukini <kealii.ohumukini@gmail.com>
2026-04-28 15:40:30 -0700

    Created countSpecialCharacters method body

--- src/PasswordUtils.java
+++ src/PasswordUtils.java
@@ -81,7 +81,15 @@ public class PasswordUtils {
     public static int countSpecialCharacters(String password) {
     // TODO: Make tests FIRST, then implement the method
     // Please have your tests in a separate commit from the implementation
-        return 0;
+        int count = 0; 
+
+        for (int i = 0; i <= password.length() - 1; i++) {
+            char c = password.charAt(i); 
+
+            if (!Character.isLetterOrDigit(c)) count++; 
+        }
+
+        return count; 
     }
 
 }
